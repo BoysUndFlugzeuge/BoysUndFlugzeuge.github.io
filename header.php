d0c7a54890c6a70490dce947225bbf0295abc9d6
Cedric VU <114571271+Cedvinvu@users.noreply.github.com>
2022-11-23 20:16:42 +0100

    testing php

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<p>Vul de gegevens in:</p>
+<form name="form1" method="post" action="Oef1_2.php">
+  <p>Geef het verkoopcijfer 
+    <input name="txtCommissie" type="text" id="txtNaam" size="25" maxlength="30">
+</p>
+  <p>
+    <input name="cmdVerstuur" type="submit" id="cmdVerstuur" value="Bereken commissieloon">
+    <input type="reset" name="Reset" value="Opnieuw">
+  </p>
+</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>BuF</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="https://boysundflugzeuge.github.io/style.css">
+<link rel="icon" type="image/x-icon" href="https://boysundflugzeuge.github.io/favicon.png">
+</head>
+<body>
+<div class="main">
+<header>
+ <h1>Boys und Flugzeuge</h1>
+ </header>
+ <nav>
+ <ul>
+  <li><a href="https://boysundflugzeuge.github.io/index.html">Main menu</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/stories.html">Stories</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/news.html">News</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/wiki.html">Wiki</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/extra.html">Extra</a></li>
+ </ul>
+ </nav>
+ <aside>
+ <ul>
+  <li><a href="#About">About us</a></li>
+ </ul>
+ </aside>
+ <article>
+  <p>Hello pilot, welcome to the Boys und Flugzueg website!</p>
+  <h2 id="about">About us</h2>
+  <p>We are a group of Girls und Panzer enthusiasts who felt that the series had room for expansion (And not in the conventional fanfiction manner).</p>
+  <p>Girls in tanks was a pretty cool concept, then we got thinking...<br>"What if there were boys in planes too?"<br>"That would be pretty cool."</p>
+  <p>Our collective group consists of regular people like yourself, people who have crazy ideas and a hankering for more content out of our favorite animes.<br>We are students, workers, wage slaves and freeloaders who wanted to put out a collective piece of artwork that an entire community of people can enjoy.</p>
+  <p>This project has been a large part of our lives, dragging us through thick and thin, highs and lows.<br>Through constant perseverance (And a collective council of leaders) we have managed to slam together what we believe to be our groups best work.</p>
+
+  <p>These past few years have been challenging for all involved so we implore you, our readers, to give us some feedback on our <a href="https://discord.gg/WWHfjkFmnW">discord server</a>, it'd be pretty nice if you could.</p>
+
+  <p>I hope you enjoy your stay and our work.</p>
+ </article>
+ <footer>
+ <p><a href="https://discord.gg/WWHfjkFmnW">Join our discord</a></p>
+ </footer>
+</div>
+</body>
+</html>
