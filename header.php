16458d8c4bc426b4d319fcb0ff824d16dd54f85a
Cedric VU <114571271+Cedvinvu@users.noreply.github.com>
2022-11-23 20:17:19 +0100

    Update header.php

--- header.php
+++ header.php
@@ -5,15 +5,6 @@
 </head>
 
 <body>
-<p>Vul de gegevens in:</p>
-<form name="form1" method="post" action="Oef1_2.php">
-  <p>Geef het verkoopcijfer 
-    <input name="txtCommissie" type="text" id="txtNaam" size="25" maxlength="30">
-</p>
-  <p>
-    <input name="cmdVerstuur" type="submit" id="cmdVerstuur" value="Bereken commissieloon">
-    <input type="reset" name="Reset" value="Opnieuw">
-  </p>
-</form>
+
 </body>
 </html>
