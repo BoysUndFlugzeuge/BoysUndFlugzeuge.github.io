9c7443787644c23fc59c32f364034c2d8a801b68
Cedric VU <114571271+Cedvinvu@users.noreply.github.com>
2022-11-23 20:20:26 +0100

    php test

--- header.php
+++ header.php
@@ -5,6 +5,17 @@
 </head>
 
 <body>
-
+<header>
+ <H1><img src="https://boysundflugzeuge.github.io/logo.png" width="291" height="130"><H1>
+ </header>
+ <nav>
+ <ul>
+  <li><a href="https://boysundflugzeuge.github.io/index.php">Main menu</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/stories.php">Stories</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/news.php">News</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/wiki.php">Wiki</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/extra.php">Extra</a></li>
+ </ul>
+ </nav>
 </body>
 </html>
---
+++ stories.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>BuF</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="https://boysundflugzeuge.github.io/style.css">
+<link rel="icon" type="image/x-icon" href="https://boysundflugzeuge.github.io/favicon.png">
+</head>
+<body>
+<div class="main">
+<? 
+    include("header.php");
+?>
+ <aside>
+ <ul>
+  <li><a href="#other">Other stories</a></li>
+ </ul>
+ </aside>
+ <article>
+ <h3><a href="https://boysundflugzeuge.github.io/stories/BuF/mainBuF.html">BuF main story</a></h3>
+ <hr>
+ <h1 id="other">Other Boys und Flugzeug stories</h1>
+ <h3><a href="https://boysundflugzeuge.github.io/stories/DragonOfOoarai.html">Dragon of Oarai</a></h3>
+ </article>
+ <footer>
+ <p><a href="https://discord.gg/WWHfjkFmnW">Join our discord</a></p>
+ </footer>
+</div>
+</body>
+</html>
