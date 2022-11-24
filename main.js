f01a6293fe25acf6e05b6fbdaa36e7b5464ae45a
Cedric VU <114571271+Cedvinvu@users.noreply.github.com>
2022-11-24 21:41:52 +0100

    Revert "doing the footer too"
    This reverts commit 4b805473f2d051960e8c0005a19f1582da4ef2cb.

--- main.js
+++ main.js
@@ -16,14 +16,4 @@ class MyHeader extends HTMLElement {
  }
 }
 
-customElements.define('Mheader',MyHeader)
-
-class MyFooter extends HTMLElement {
- connectedCallback() {
-  this.innerHTML = `<footer>
-<p><a href="https://discord.gg/WWHfjkFmnW">Join our discord</a></p></div>
-</footer>`
- }
-}
-
-customElements.define('Mfooter',MyFooter)
+customElements.define('my-header',MyHeader)
