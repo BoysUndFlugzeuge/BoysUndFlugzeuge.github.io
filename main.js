211ca05a55f788df185b59bdef35d03bf96fef54
Cedric VU <114571271+Cedvinvu@users.noreply.github.com>
2022-11-24 21:26:04 +0100

    testing javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+class MyHeader extends HTMLElement {
+ connectedCallback() {
+  this.innerHTML = `<header>
+ <H1><img src="https://boysundflugzeuge.github.io/logo.png" width="291" height="130"><H1>
+ </header>
+ <nav>
+ <ul>
+  <li><a href="https://boysundflugzeuge.github.io/index.html">Main menu</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/stories.html">Stories</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/news.html">News</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/friday.html">Fridays</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/wiki.html">Wiki</a></li>
+  <li><a href="https://boysundflugzeuge.github.io/extra.html">Extra</a></li>
+ </ul>
+ </nav>`
+ }
+}
+
+customElements.define('my-header',MyHeader)
